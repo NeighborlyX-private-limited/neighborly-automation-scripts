27b85d6bc831c6a2145fbea4c02864bf062ff3f4
Kushal Omer <kushalomer06@gmail.com>
2024-10-14 13:16:07 +0530

    Added try-catch and logs

--- AwardScript/awardScript.py
+++ AwardScript/awardScript.py
@@ -24,22 +24,24 @@ collection = db[DB_COLLECTION]
 # Query all documents
 results = collection.find()
 
-# List of award types
-awardTypes = ['Local Legend', 'Sunflower', 'Streetlight', 'Park Bench', 'Map']
-
 # Iterate through the documents and print each one
 for document in results:
-    document_id = document['_id']
-    flag = True
-    for award in awardTypes:
-        if document['awards'][award] != 0:
-            flag = False
-            break
-    if flag:
-        index = random.randrange(0,5)
-        collection.update_one(
-            {"_id": document_id},
-            {"$inc": {"awards."+awardTypes[index]: 1}}
-        )
-        updated_doc = collection.find_one({"_id": ObjectId(document_id)})
-        print(updated_doc)
+    try:
+        document_id = document['_id']
+        flag = True
+        awardTypes = list(document['awards'].keys())
+        size = len(awardTypes)
+        for award in awardTypes:
+            if document['awards'][award] != 0:
+                flag = False
+                break
+        if flag:
+            index = random.randrange(0, size)
+            collection.update_one(
+                {"_id": document_id},
+                {"$inc": {"awards."+awardTypes[index]: 1}}
+            )
+            updated_doc = collection.find_one({"_id": ObjectId(document_id)})
+            print("Award granted to: ", updated_doc)
+    except:
+        print("Something wrong with userid:", document['_id'])
