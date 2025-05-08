9f01cc50f27abd011a8ffbbeb71f6ae961d4e54a
Vishal-44 <vraj70404@gmail.com>
2025-05-08 21:02:02 +0530

    fixes in backfilling and code generator.

--- InviteCodeScript/utils/CodeGenerator.js
+++ InviteCodeScript/utils/CodeGenerator.js
@@ -1,9 +1,13 @@
-const { customAlphabet } = require("nanoid")
 const { VALID_CHARACTER_TO_GENERATE_INVITE_CODE } = require("../constants")
+const crypto = require("crypto")
 
 const generateCode = () => {
-    const CodeGenerator = customAlphabet(VALID_CHARACTER_TO_GENERATE_INVITE_CODE, 8)
-    const code = CodeGenerator()
+    const length = 8;
+    let code = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = crypto.randomInt(0,  VALID_CHARACTER_TO_GENERATE_INVITE_CODE.length);
+        code += VALID_CHARACTER_TO_GENERATE_INVITE_CODE[randomIndex];
+    }
     return code
 }
 
